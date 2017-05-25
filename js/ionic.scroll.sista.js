1b8b2d797373b6e4294c2a9a0e84a60f88912958
Emily Morehouse <emily@cuttlesoft.com>
2017-05-25 10:44:35 -0600

    Adding validation for .scrollTop to avoid unnecessary console errors.

--- js/ionic.scroll.sista.js
+++ js/ionic.scroll.sista.js
@@ -275,7 +275,7 @@
             e = e.originalEvent || e;
 
             var duration = 0;
-            var scrollTop = e.detail.scrollTop;
+            var scrollTop = angular.isDefined(e.detail) ? e.detail.scrollTop : null;
 
             y = scrollTop >= 0 ? Math.min(defaultEnd, Math.max(0, y + scrollTop - prevScrollTop)) : 0;
 
